7a50fa57c36062c5b0172822556c134ab81e51c9
develar <develar@gmail.com>
2016-11-19 16:53:44 +0100

    fix: remove squirrel handler

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,28 @@
-import { app, ipcMain } from "electron"
-import setMenu from "./AppMenuManager"
-import { log } from "./util"
-import WindowManager from "./WindowManager"
-import handleStartupEvent from "./WinSquirrelStartupEventHandler"
+import {app, ipcMain} from "electron";
+import setMenu from "./AppMenuManager";
+import {log} from "./util";
+import WindowManager from "./WindowManager";
 
 let windowManager: WindowManager = null
-if (!handleStartupEvent()) {
-  if (app.makeSingleInstance((commandLine: any[], workingDirectory: string) => {
-      // someone tried to run a second instance, we should focus our window
-      if (windowManager != null) {
-        windowManager.focusFirstWindow()
-      }
-      return true
-    })) {
-    app.quit()
-  }
-  else {
-    require("electron-debug")()
+if (app.makeSingleInstance((commandLine: any[], workingDirectory: string) => {
+    // someone tried to run a second instance, we should focus our window
+    if (windowManager != null) {
+      windowManager.focusFirstWindow()
+    }
+    return true
+  })) {
+  app.quit()
+}
+else {
+  require("electron-debug")()
 
-    app.on("ready", () => {
-      ipcMain.on("log.error", (event: any, arg: any) => {
-        log(arg)
-      })
-
-      setMenu("https://cad.onshape.com/documents")
-      windowManager = new WindowManager()
-      windowManager.openWindows()
+  app.on("ready", () => {
+    ipcMain.on("log.error", (event: any, arg: any) => {
+      log(arg)
     })
-  }
+
+    setMenu("https://cad.onshape.com/documents")
+    windowManager = new WindowManager()
+    windowManager.openWindows()
+  })
 }
