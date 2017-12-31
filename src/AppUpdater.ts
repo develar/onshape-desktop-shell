675ec6d9b149866bfdafad30e3d251047e31b31c
develar <develar@gmail.com>
2017-12-31 18:40:27 +0100

    chore: update deps

--- src/AppUpdater.ts
+++ src/AppUpdater.ts
@@ -3,7 +3,7 @@ import { autoUpdater } from "electron-updater"
 export default class AppUpdater {
   constructor() {
     const log = require("electron-log")
-    log.transports.file.level = "info"
+    log.transports.file.level = "debug"
     autoUpdater.logger = log
     autoUpdater.checkForUpdatesAndNotify()
   }
